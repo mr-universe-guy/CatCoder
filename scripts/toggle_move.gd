4f0e1aca1f2418e7c78e94e6763f1547ae35c04f
Matthew Universe <ff8loser@gmail.com>
2026-03-02 14:13:12 -0500

    ToggleMove script now uses local transform and tweens the values when not in the editor

--- scripts/toggle_move.gd
+++ scripts/toggle_move.gd
@@ -7,14 +7,28 @@ extends Node3D
 	set = _on_toggle
 
 @export var toggle_transform := Transform3D()
+var tween : Tween
 
 ## The current transform becomes the toggle transform and the toggle transform becomes the current transform
 func _on_toggle(value : bool) -> void:
+	if tween and tween.is_running():
+		return
+	
 	toggle = value
-	var temp := Transform3D(global_transform)
-	global_transform = toggle_transform
-	toggle_transform = temp
+	var current := Transform3D(transform)
+	
+	if Engine.is_editor_hint():
+		transform = toggle_transform
+		toggle_transform = current
+	else:
+		tween = self.create_tween()
+		tween.tween_property(self, "transform", toggle_transform, 1.0).from_current()
+		tween.tween_callback(_finalize_transform.bind(current))
+		tween.play()
 
 
-func _on_interact(player: Player) -> void:
+func _finalize_transform(trans: Transform3D) -> void:
+	toggle_transform = trans
+
+func _on_interact(_player: Player) -> void:
 	toggle = !toggle
